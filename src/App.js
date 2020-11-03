9ab9a8989580cc0f83c3891e718f204325771af8
weilyuwang <weilyuwang@gmail.com>
2020-11-03 16:52:32 -0500

    :fire: init components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Articles from "./components/Articles";
+import User from "./components/User";
 
 function App() {
   return (
@@ -6,7 +8,10 @@ function App() {
       <header>
         <h1>React Skeletons</h1>
       </header>
-      <div className="content"></div>
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
     </div>
   );
 }
---
+++ src/components/Articles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Articles = () => {
+  return (
+    <div>
+      <h2>Articles</h2>
+    </div>
+  );
+};
+
+export default Articles;
